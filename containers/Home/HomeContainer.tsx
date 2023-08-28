99eef7dba5134be01c74178951e9c5d007f90914
parkhyeonji <iamphj3@naver.com>
2023-08-28 18:56:37 +0900

    design : theme 적용 테스트

--- containers/Home/HomeContainer.tsx
+++ containers/Home/HomeContainer.tsx
@@ -1,14 +1,23 @@
+import { styled } from 'styled-components';
+
 import { Header } from '@/components/Common/Header';
 import { Home } from '@/components/Home';
 
 const index = () => {
   return (
-    <>
+    <StHomeContainer>
       <p>홈 페이지 컨테이너</p>
       <Header />
       <Home />
-    </>
+    </StHomeContainer>
   );
 };
 
 export default index;
+
+const StHomeContainer = styled.main`
+  & > p {
+    color: ${({ theme }) => theme.colors.White};
+    ${({ theme }) => theme.fonts.Body0};
+  }
+`;
