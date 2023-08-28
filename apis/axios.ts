e072ff65f1631406d89febe744fad390a902e78a
parkhyeonji <iamphj3@naver.com>
2023-08-28 17:38:48 +0900

    chore : apis 폴더 세팅

---
+++ apis/axios.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_APP_IP,
+  headers: {
+    'Content-type': 'application/json',
+    'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_APP_IP,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+  },
+});
+
+export { client };
