923faeb25b91c0eb401aa12af950d69eb46111a0
parkhyeonji <iamphj3@naver.com>
2023-08-31 12:21:45 +0900

    #1 design : 헤더 모달 구현

--- components/Common/Header/HeaderModal.tsx
+++ components/Common/Header/HeaderModal.tsx
@@ -8,12 +8,21 @@ interface HeaderModalProps {
 
 const HeaderModal = ({ isShowing, handleHide }: HeaderModalProps) => {
   const router = useRouter();
+
+  const handleProfile = () => {
+    console.log('프로필 설정 : 1차 데모 이후 구현');
+  };
+  const handleLogout = () => {
+    console.log('로그아웃 : 1차 데모 이후 구현');
+  };
+
   return (
     isShowing && (
       <StHeaderModalWrapper>
-        <button type="button" onClick={}>
+        <button type="button" onClick={handleProfile}>
           프로필 설정
         </button>
+        <hr />
         <button
           type="button"
           onClick={() => {
@@ -22,7 +31,8 @@ const HeaderModal = ({ isShowing, handleHide }: HeaderModalProps) => {
         >
           마이페이지
         </button>
-        <button type="button" onClick={}>
+        <hr />
+        <button type="button" onClick={handleLogout}>
           logout
         </button>
       </StHeaderModalWrapper>
@@ -33,8 +43,29 @@ const HeaderModal = ({ isShowing, handleHide }: HeaderModalProps) => {
 export default HeaderModal;
 
 const StHeaderModalWrapper = styled.div`
-  width: 7rem;
-  height: 5.7rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 0.3rem;
+
+  width: 10rem;
+  height: fit-content;
+  padding: 0.6rem;
+  box-sizing: border-box;
+
   border-radius: 0.5rem;
   background: ${({ theme }) => theme.colors.Purple1};
+
+  & > button {
+    color: ${({ theme }) => theme.colors.White};
+    ${({ theme }) => theme.fonts.Body2};
+  }
+  & > hr {
+    border: none;
+    border-top: 0.06rem solid ${({ theme }) => theme.colors.White};
+    width: 100%;
+  }
+  & > button:hover {
+    color: ${({ theme }) => theme.colors.Blue};
+  }
 `;
