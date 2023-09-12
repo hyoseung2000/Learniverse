ced4fc73a0a7a6dff6e8dcce238d019c0fb63ee1
parkhyeonji <iamphj3@naver.com>
2023-09-12 14:29:58 +0900

    fix :  does not match server-rendered HTML 이슈 해결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 
@@ -10,31 +10,32 @@ import { GlobalStyle } from '@/styles/GlobalStyle';
 import { theme } from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  // useEffect(() => {
-  //   function adjustPageZoom() {
-  //     const screenWidth = window.innerWidth;
-  //     const zoomRatio = 1024 / screenWidth; // MEMO :: 1920px가 기준 화면 너비
+  const [isClient, setIsClient] = useState(false);
 
-  //     document.body.style.zoom = zoomRatio;
-  //   }
-  //   adjustPageZoom();
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-  //   // 페이지 로드 시 및 화면 크기 변경 시 확대 적용
-  //   window.addEventListener('load', adjustPageZoom);
-  //   window.addEventListener('resize', adjustPageZoom);
-  // }, []);
-
-  return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Head>
-          <title>LearniVerse</title>
-          <link rel="icon" href="/favicon.png" />
-        </Head>
-        <Header />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </RecoilRoot>
-  );
+  if (!isClient) {
+    return null;
+  }
+  if (typeof window === 'undefined') {
+    return <></>;
+  } else {
+    return (
+      isClient && (
+        <RecoilRoot>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Head>
+              <title>LearniVerse</title>
+              <link rel="icon" href="/favicon.png" />
+            </Head>
+            <Header />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </RecoilRoot>
+      )
+    );
+  }
 }
