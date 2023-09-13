00a9a372d5b8faa0b1b47b7f5d44a1aef4d16382
parkhyeonji <iamphj3@naver.com>
2023-09-14 00:44:28 +0900

    chore : mediasoup 타입 가져오기

--- containers/WebRTC/WebRTCContainer.tsx
+++ containers/WebRTC/WebRTCContainer.tsx
@@ -1,10 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable jsx-a11y/media-has-caption */
 import { Device } from 'mediasoup-client';
+import {
+  Producer,
+  RtpCapabilities,
+  RtpParameters,
+  Transport,
+  UnsupportedError,
+} from 'mediasoup-client/lib/types';
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
 import { styled } from 'styled-components';
+
+import socketPromise from './socketPromise';
+
+interface CustomSocket extends Socket {
+  request?: (event: string, data?: any) => Promise<any>;
+}
 
 const WebRTCContainer = () => {
   const router = useRouter();
@@ -12,37 +27,134 @@ const WebRTCContainer = () => {
   const [curMemberId, setCurMemberId] = useState<string>();
   const [curCoreTimeId, setCurCoreTimeId] = useState<string>();
 
-  const [device, setDevice] = useState<Device | null>(null);
-  const [socket, setSocket] = useState<Socket | null>(null);
+  const [device, setDevice] = useState<Device>();
+  const [socket, setSocket] = useState<CustomSocket | null>(null);
+  const [producer, setProducer] = useState<Producer>();
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   const connect = async () => {
-    const socketConnection = await io('https://0.0.0.0:8080/', {
+    const socketConnection: CustomSocket = await io('https://0.0.0.0:8080/', {
       transports: ['websocket'],
       path: '/server',
     });
+    socketConnection.request = socketPromise(socketConnection);
     setSocket(socketConnection);
-    console.log('1. socket 연결됨', socketConnection);
+    console.log('1. socket connect', socketConnection);
   };
 
-  const join = async (name, room_id) => {
-    socket
-      .request('join', {
-        name,
-        room_id,
-      })
-      .then(async function (e) {
-        console.log('2. Joined to room', e);
+  const join = async (memberId: string, coreTimeId: string) => {
+    if (socket && socket.request) {
+      try {
+        const socketJoin = await socket.request('join', {
+          curMemberId,
+          curCoreTimeId,
+        });
+        console.log('2. Joined to room', socketJoin);
+
         const data = await socket.request('getRouterRtpCapabilities');
-        device = await loadDevice(data);
+        const device = await loadDevice(data);
         console.log('3. device 로딩 ', device);
-        // device = device;
-        await initTransports(device);
-        socket.emit('getProducers');
-      })
-      .catch((err) => {
+
+        if (device) {
+          await initTransports(device);
+          socket.emit('getProducers');
+        }
+      } catch (err) {
         console.log('Join error:', err);
-      });
+      }
+    }
+  };
+
+  const loadDevice = async (routerRtpCapabilities: RtpCapabilities) => {
+    let curDevice;
+    try {
+      curDevice = new Device();
+      await curDevice.load({ routerRtpCapabilities });
+      setDevice(curDevice);
+    } catch (error) {
+      if (error instanceof UnsupportedError) {
+        console.error('Browser not supported');
+      } else {
+        console.error(error);
+      }
+      return null;
+    }
+    return curDevice;
+  };
+
+  const createTransport = async (
+    device: Device,
+    direction: 'send' | 'recv',
+  ): Promise<Transport> => {
+    if (!socket || !socket.request) throw new Error('Socket not initialized');
+
+    const data = await socket.request('createWebRtcTransport', {
+      forceTcp: false,
+      rtpCapabilities:
+        direction === 'send' ? device.rtpCapabilities : undefined,
+    });
+    if (data.error) {
+      console.error(data.error);
+      throw new Error(data.error);
+    }
+
+    const transport = device.createSendTransport(data);
+
+    transport.on('connect', async ({ dtlsParameters }, callback, errback) => {
+      try {
+        if (!socket.request) return;
+        await socket.request('connectTransport', {
+          transport_id: transport.id,
+          dtlsParameters,
+        });
+        callback();
+      } catch (err) {
+        errback(err as Error);
+      }
+    });
+
+    transport.on('connectionstatechange', (state) => {
+      switch (state) {
+        case 'connected':
+          console.log(`${direction} transport connected`);
+          break;
+        case 'failed':
+          transport.close();
+          break;
+        default:
+          break;
+      }
+    });
+
+    if (direction === 'send') {
+      transport.on(
+        'produce',
+        async ({ kind, rtpParameters }, callback, errback) => {
+          try {
+            if (!socket.request) return;
+            const { producerId } = await socket.request('produce', {
+              producerTransportId: transport.id,
+              kind,
+              rtpParameters,
+            });
+            callback({ id: producerId });
+          } catch (err) {
+            errback(err as Error);
+          }
+        },
+      );
+    }
+
+    return transport;
+  };
+
+  const initTransports = async (device: Device) => {
+    try {
+      const producerTransport = await createTransport(device, 'send');
+      const consumerTransport = await createTransport(device, 'recv');
+    } catch (err) {
+      console.error('Failed to initialize transports:', err);
+    }
   };
 
   useEffect(() => {
