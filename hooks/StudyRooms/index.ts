cd3c31319096a1fbfc1464d012ef9552687d4719
parkhyeonji <iamphj3@naver.com>
2023-10-05 15:43:39 +0900

    refactor : 마이페이지 스터디룸 swr로 리팩토링

--- hooks/StudyRooms/index.ts
+++ hooks/StudyRooms/index.ts
@@ -1,1 +1,3 @@
 export { default as useGetMyStudyRoomList } from './useGetMyStudyRoomList';
+export { default as useGetLeaderStudyRoomList } from './useGetLeaderStudyRoomList';
+export { default as useGetApplyStudyRoomList } from './useGetApplyStudyRoomList';
