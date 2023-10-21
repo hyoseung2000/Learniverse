7ffeb019ae28ba0ba51f3980b1649e45c9c37bd6
parkhyeonji <iamphj3@naver.com>
2023-10-21 14:24:20 +0900

    fix : 스터디룸 roomId 주소 변경 시 가져오기

--- containers/Studyroom/StudyroomContainer.tsx
+++ containers/Studyroom/StudyroomContainer.tsx
@@ -1,8 +1,22 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
 import { styled } from 'styled-components';
 
 import { Studyroom } from '@/components/Studyroom';
+import { roomIdState } from '@/recoil/atom';
 
 const StudyroomContainer = () => {
+  const router = useRouter();
+  const setRoomId = useSetRecoilState(roomIdState);
+
+  useEffect(() => {
+    const matched = router.asPath.match(/\/studyroom\/(\d+)/);
+    if (matched && matched[1]) {
+      setRoomId(Number(matched[1]));
+    }
+  }, [router.asPath]);
+
   return (
     <StStudyroomWrapper>
       <Studyroom />
